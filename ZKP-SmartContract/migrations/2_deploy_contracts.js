d649ceed655daa2d538a1899690b61f303601c8a
Zahraa A. AlAbdullah <109365947+Possibly-Necessary@users.noreply.github.com>
2024-07-05 09:10:20 +0300

    Update 2_deploy_contracts.js

--- ZKP-SmartContract/migrations/2_deploy_contracts.js
+++ ZKP-SmartContract/migrations/2_deploy_contracts.js
@@ -1,24 +1,4 @@
 
-/*
-const Groth16Verifier = artifacts.require("Groth16Verifier");
-const Coin = artifacts.require("Coin");
-const SecretKeeper = artifacts.require("SecretKeeper");
-
-module.exports = function(deployer) {
-    // Parameters for deployment
-    const rewardAmount = web3.utils.toWei("1", "ether");  // Assuming 1 ETH as reward amount
-    let verifierAddress, coinAddress; // Define the address variables
-
-    deployer.deploy(Groth16Verifier).then(function(instance) { // 'then' is used to ensure the order of the deployed contracts
-        verifierAddress = instance.address; // Creating an instance 
-        return deployer.deploy(Coin, rewardAmount);
-    }).then(function(coinInstance) {
-        coinAddress = coinInstance.address;
-        return deployer.deploy(SecretKeeper, verifierAddress, rewardAmount, coinAddress);
-    });
-};
-*/
-
 // Declare the constant variables 'Groth16Verifier', 'Coin' and 'SecretKeeper'
 const Groth16Verifier = artifacts.require("Groth16Verifier"); // For Truffle to load compiled contracts from the artifacts found in the build folder
 const Coin = artifacts.require("Coin");
